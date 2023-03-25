b7930a5287b555a7088508b92229787595293deb
GeneralQuarter <x1q.g1x@gmail.com>
2023-03-25 01:49:58 +0100

    feat: allow multiple cors origins

--- lib/with-cors.ts
+++ lib/with-cors.ts
@@ -2,8 +2,11 @@ import { VercelRequest, VercelResponse } from '@vercel/node'
 
 
 const withCors = <T>(handler: (req: VercelRequest, res: VercelResponse) => Promise<T>) => async (req: VercelRequest, res: VercelResponse) => {
+  const allowedOrigins = (process.env.TREEVIEWER_ALLOWED_ORIGINS ?? '').split(',');
+  const origin = allowedOrigins.find(o => req.headers.origin === o);
+
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  res.setHeader('Access-Control-Allow-Origin', process.env.TREEVIEWER_CORS ?? '');
+  res.setHeader('Access-Control-Allow-Origin', origin ?? '');
   res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
   res.setHeader(
     'Access-Control-Allow-Headers',
